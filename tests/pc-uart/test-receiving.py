d1ebf0968f75ce9899ef9853e72ec8140e169f3e
Djordje Vujic <djordjevujic@yahoo.com>
2018-01-16 13:58:47 +0100

    uart-packet-receive-print

--- tests/pc-uart/test-receiving.py
+++ tests/pc-uart/test-receiving.py
@@ -1,10 +1,20 @@
 import serial
 
-ser = serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout=2.0)  # open serial port
 
-print('name: ' +ser.name)         # check which port was really used
+# convert string to hex
+def to_hex(s):
+    lst = []
+    for ch in s:
+        hv = hex(ch)
+        if len(hv) == 1:
+            hv = '0' + hv
+        lst.append(hv)
+
+    return lst
+
+ser = serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout=0.5)  # open serial port
+print('name: ' + ser.name)  # check which port was really used
 
 while True:
     s = ser.read(10)
-    print(s)
-
+    print(to_hex(s))
